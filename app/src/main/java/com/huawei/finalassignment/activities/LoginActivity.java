0b06dc9cfd6953ec4f201588c6adedeb2611c9e2
nkhoi2000 <nkhoi2000@gmail.com>
2021-12-17 14:33:19 +0700

    Update login

--- app/src/main/java/com/huawei/finalassignment/activities/LoginActivity.java
+++ app/src/main/java/com/huawei/finalassignment/activities/LoginActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.huawei.finalassignment.R;
@@ -25,13 +27,21 @@ public class LoginActivity extends AppCompatActivity {
     // Define the log tag.
     private static final String TAG = "Account";
     private Account account;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        Button btn_login = findViewById(R.id.btn_login_username);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Cannot find your username & password!", Toast.LENGTH_SHORT).show();
+            }
+        });
         findViewById(R.id.btn_login).setOnClickListener(view -> silentSignInByHwId());
     }
+
     private void silentSignInByHwId() {
         // 1. Use AccountAuthParams to specify the user information to be obtained, including the user ID (OpenID and UnionID), email address, and profile (nickname and picture).
         // 2. By default, DEFAULT_AUTH_REQUEST_PARAM specifies two items to be obtained, that is, the user ID and profile.
@@ -45,17 +55,17 @@ public class LoginActivity extends AppCompatActivity {
         // Use silent sign-in to sign in with a HUAWEI ID.
         Task<AuthAccount> task = mAuthService.silentSignIn();
         task.addOnSuccessListener(authAccount -> {
-            Intent intent = new Intent(LoginActivity.this,MapActivity.class);
-            account = new Account(authAccount.getDisplayName(),authAccount.getEmail(),authAccount.getAvatarUri().toString());
-            intent.putExtra("Account",account);
+            Intent intent = new Intent(LoginActivity.this, MapActivity.class);
+            account = new Account(authAccount.getDisplayName(), authAccount.getEmail(), authAccount.getAvatarUri().toString());
+            intent.putExtra("Account", account);
             startActivity(intent);
-            Toast.makeText(LoginActivity.this,"Hello " + authAccount.getDisplayName(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(LoginActivity.this, "Hello " + authAccount.getDisplayName(), Toast.LENGTH_SHORT).show();
         });
         task.addOnFailureListener(e -> {
             // The silent sign-in fails. Use the getSignInIntent() method to show the authorization or sign-in screen.
             if (e instanceof ApiException) {
                 Intent signInIntent = mAuthService.getSignInIntent();
-                Log.i(TAG, "FAIL: " +  e.getMessage() );
+                Log.i(TAG, "FAIL: " + e.getMessage());
                 startActivityForResult(signInIntent, REQUEST_CODE_SIGN_IN);
             }
         });
